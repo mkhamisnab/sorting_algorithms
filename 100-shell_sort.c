2dc376ddc367959536e5b9f3f7547e56042cf3ba
Nabil <mkhamisnabil@gmail.com>
2023-07-22 08:49:00 -0700

    Created 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+*swap - the positions of two elements into a array
+*@array: array
+*@element1: element in array
+*@element2: element in array
+*/
+void swap(int *array, int element1, int element2)
+{
+
+	int tmp;
+
+	tmp = array[element1];
+	array[element1] = array[element2];
+	array[element2] = tmp;
+}
+/**
+ * shell_sort - function that sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
+ * @size: size of the array
+ * @array: list with numbers
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, index = 0;
+
+	if (array == NULL || size < 2)
+		return;
+	while (gap < size / 3)
+		gap = 3 * gap + 1;
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+			for (index = i; index >= gap &&
+			 (array[index] < array[index - gap]); index -= gap)
+				swap(array, index, index - gap);
+		print_array(array, size);
+		gap /= 3;
+	}
+}
